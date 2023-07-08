03b200b600bd4f073399f22b9aa6c342ec34ca99
Kian Candelario <kiancandelario1@gmail.com>
2023-07-08 21:00:38 +0800

    feat: Added responsiveness to the circle

--- Newbie/Results-Summary/resultsSummary/src/components/Circle.tsx
+++ Newbie/Results-Summary/resultsSummary/src/components/Circle.tsx
@@ -1,8 +1,8 @@
 function Circle() {
     return (
         <>
-            <div className="w-44 h-44 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full flex flex-col gap-1 justify-center items-center my-6">
-                <div className="font-bold text-6xl text-white">
+            <div className="w-40 h-40 sm:w-36 sm:h-36 md:w-40 md:h-40 2xl:w-44 2xl:h-44 bg-gradient-to-b from-violet-blue to-persian-blue rounded-full flex flex-col gap-1 justify-center items-center my-6">
+                <div className="font-bold text-6xl sm:text-5xl md:text-6xl text-white">
                     <h1>76</h1>
                 </div>
                 <div className="text-light-lavender font-semibold">
