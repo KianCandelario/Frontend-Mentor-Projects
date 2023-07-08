12f0e027f2be6cd563f24bee2f6d5273fb20c9ea
Kian Candelario <kiancandelario1@gmail.com>
2023-07-08 15:10:04 +0800

    style: Adjusted the flex gap

--- Newbie/Results-Summary/resultsSummary/src/components/App.tsx
+++ Newbie/Results-Summary/resultsSummary/src/components/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className='w-screen h-screen flex justify-center items-center bg-pale-blue'>
-        <div className='w-full h-full rounded-b-3xl flex flex-col bg-slate-50 sm:flex sm:flex-row sm:gap-1 sm:h-3/5 sm:w-4/5 sm:drop-shadow-2xl sm:rounded-3xl md:h-3/5 md:w-3/4 md:gap-7 lg:w-3/5 lg:h-3/5 xl:w-1/2 xl:h-3/5 2xl:w-5/12 2xl:h-3/5'>
+        <div className='w-full h-full rounded-b-3xl flex flex-col bg-slate-50 sm:flex sm:flex-row sm:gap-1 sm:h-3/5 sm:w-4/5 sm:drop-shadow-2xl sm:rounded-3xl md:h-3/5 md:w-3/4 lg:w-3/5 lg:h-3/5 xl:w-1/2 xl:h-3/5 2xl:w-5/12 2xl:h-3/5'>
           <Result />
           <SummaryContainer />
         </div>
