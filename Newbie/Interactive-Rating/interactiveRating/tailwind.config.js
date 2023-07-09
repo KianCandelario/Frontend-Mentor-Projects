3a6ce2b1675e2dbe6167c812e0cc21c843240d5a
Kian Candelario <kiancandelario1@gmail.com>
2023-07-09 23:19:54 +0800

    feat: Added custom colors and font

--- Newbie/Interactive-Rating/interactiveRating/tailwind.config.js
+++ Newbie/Interactive-Rating/interactiveRating/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)'
+      },
+      fontFamily: {
+        'overpass': ['Overpass', 'sans-serif'] 
+      },
+    },
   },
   plugins: [],
 }
