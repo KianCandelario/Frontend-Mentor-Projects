5c9173c90bbf39a1d3246c8fb2f03eedb39bab80
Kian Candelario <kiancandelario1@gmail.com>
2023-07-09 23:46:09 +0800

    fix: Corrected the icon path

--- Newbie/Interactive-Rating/interactiveRating/src/components/Contents.tsx
+++ Newbie/Interactive-Rating/interactiveRating/src/components/Contents.tsx
@@ -6,10 +6,10 @@ function Contents() {
             <div className='px-6 font-overpass flex flex-col gap-4'>
                 <div>
                     <div className="bg-medium-grey/10 p-2 w-11 h-11 rounded-full flex justify-center items-center">
-                        <img src="../../public/assets/icons/icon-star.svg" />
+                        <img src="../../assets/icons/icon-star.svg" />
                     </div>
                 </div>
-                <div className="">
+                <div>
                     <div className="font-bold text-2xl text-white mb-2">
                         <h1>How did we do?</h1>
                     </div>
