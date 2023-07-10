f1334818b5b8ffa9f554897b892896228456b052
Kian Candelario <kiancandelario1@gmail.com>
2023-07-10 20:29:45 +0800

    feat: Added responsiveness

--- Newbie/Interactive-Rating/interactiveRating/src/components/Container.tsx
+++ Newbie/Interactive-Rating/interactiveRating/src/components/Container.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center bg-very-dark-blue">
-        <div className="h-[55%] w-[87%] bg-dark-blue rounded-2xl flex justify-center items-center">
+        <div className="h-[55%] w-[87%] sm:w-[50%] md:w-[45%] lg:w-[30%] 2xl:w-[25%] bg-dark-blue rounded-2xl flex justify-center items-center">
           <Contents />
         </div>
       </div>
