ebf93cfd55b30135ff45478a044f795dd7c2e898
Kian Candelario <kiancandelario1@gmail.com>
2023-07-14 20:24:07 +0800

    style: Finished the layout

--- Newbie/Interactive-Rating/interactiveRating/src/components/Submitted.tsx
+++ Newbie/Interactive-Rating/interactiveRating/src/components/Submitted.tsx
@@ -5,16 +5,16 @@ type Rating = {
 function Submitted(props: Rating) {
     return(
         <>
-            <div>
+            <div className="flex flex-col justify-center items-center font-overpass px-4">
                 <div>
                     <img src="../assets/icons/illustration-thank-you.svg" />
                 </div>
-                <div>
+                <div className="bg-medium-grey/20 text-orange/80 py-2 px-3 my-5 text-xs rounded-full">
                     <p>You selected {props.rating} out of 5</p>
                 </div>
-                <div>
-                    <h1>Thank You!</h1>
-                    <p>
+                <div className="flex flex-col justify-center items-center">
+                    <h1 className="text-white text-2xl mb-3 mt-3 font-bold">Thank You!</h1>
+                    <p className="text-center text-sm text-light-grey px-1">
                         We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
                     </p>
                 </div>
