7ddaebbc82dc348d92f5a07df0755f1b61167b64
Kian Candelario <kiancandelario1@gmail.com>
2023-07-16 00:56:45 +0800

    style: Started layouting

--- Newbie/FAQ-Accordion/faqAccordion/src/components/Container.tsx
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Container.tsx
@@ -1,9 +1,13 @@
-function App() {
+import Contents from './Contents.tsx';
+
+function Container() {
   return (
     <>
-
+      <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-b from-soft-violet-g to-soft-blue-g">
+        <Contents />
+      </div>
     </>
   )
 }
 
-export default App
+export default Container;
