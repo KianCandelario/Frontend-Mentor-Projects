8e1562f25f4348c4c038b872d2b44e25d0542257
Kian Candelario <kiancandelario1@gmail.com>
2023-07-15 22:13:16 +0800

    feat: Added custom colors and font

--- Newbie/FAQ-Accordion/faqAccordion/tailwind.config.js
+++ Newbie/FAQ-Accordion/faqAccordion/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vdark-desaturated-blue' : 'hsl(238, 29%, 16%)',
+        'soft-red' : 'hsl(14, 88%, 65%)',
+        'soft-violet-g' : 'hsl(273, 75%, 66%)',
+        'soft-blue-g' : 'hsl(240, 73%, 65%)',
+        'vdark-grayish-blue' : 'hsl(237, 12%, 33%)',
+        'dark-grayish-blue' : 'hsl(240, 6%, 50%)',
+        'light-grayish-blue' : 'hsl(240, 5%, 91%)'
+      },
+      fontFamily: {
+        'kumbh-sans' : ['Kumbh Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
