1145d42a12c4d88f67657f90f4c58a184aab69af
Kian Candelario <kiancandelario1@gmail.com>
2023-07-16 00:54:51 +0800

    feat: Added new components

---
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Contents.tsx
@@ -0,0 +1,13 @@
+import Icon from './Icon.tsx';
+
+function Contents() {
+    return (
+        <>
+            <div className="w-[90%] h-[70%] bg-white rounded-3xl">
+                <Icon />
+            </div>
+        </>
+    );
+}
+
+export default Contents;
---
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Icon.tsx
@@ -0,0 +1,12 @@
+function Icon() {
+    return(
+        <>
+            <div className="">
+                <img className="" src="./assets/icons/illustration-woman-online-mobile.svg" />
+                <img className="" src="./assets/icons/bg-pattern-mobile.svg" />
+            </div>
+        </>
+    );
+}
+
+export default Icon;
