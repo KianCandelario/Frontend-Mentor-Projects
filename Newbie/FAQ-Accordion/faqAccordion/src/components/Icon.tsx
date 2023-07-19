c9419cd508d4854ba30b6fd5d49fd647445ae10e
Kian Candelario <kiancandelario1@gmail.com>
2023-07-19 21:13:35 +0800

    feat: Started doing the icon for desktop

--- Newbie/FAQ-Accordion/faqAccordion/src/components/Icon.tsx
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Icon.tsx
@@ -1,13 +1,33 @@
 function Icon() {
     return(
         <>
-            <div className="w-3/5 h-3/5 absolute left-[20%] bottom-[44%]">
-                <div className="relative h-full w-full flex justify-center items-center">
+            <div className="w-3/5 h-3/5 absolute left-[20%] bottom-[44%] lg:top-[25%]">
+                <div className="relative h-full w-full flex justify-center items-center lg:right-[50%]">
                     <div className="z-10 h-60 w-60 absolute">
-                        <img src="./assets/icons/illustration-woman-online-mobile.svg" />
+                        <picture>
+                            <source
+                                media="(min-width: 1024px)"
+                                srcSet="./assets/icons/illustration-woman-online-desktop.svg"
+                            />
+                            <img src="./assets/icons/illustration-woman-online-mobile.svg" />
+                        </picture>
+                        <picture>
+                            <source 
+                                media="(min-width: 1024px)"
+                                srcSet="./assets/icons/illustration-box-desktop.svg"
+                            />
+                            <img src="" />
+                        </picture>
                     </div>
-                    <div className="z-0 h-60 w-60 absolute top-[48%]">
-                        <img src="./assets/icons/bg-pattern-mobile.svg" />
+                    <div className="z-0 h-60 w-60 absolute top-[48%] lg: ">
+                        <picture>
+                            <source 
+                                media="(min-width: 1024px)"
+                                srcSet="./assets/icons/bg-pattern-desktop.svg"
+                            />
+
+                            <img src="./assets/icons/bg-pattern-mobile.svg" />
+                        </picture>
                     </div>
                 </div>
             </div>
