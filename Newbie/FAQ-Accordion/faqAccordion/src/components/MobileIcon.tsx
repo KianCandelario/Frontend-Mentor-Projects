87c45e36da033eb86dfae0c002f6354667d53f44
Kian Candelario <kiancandelario1@gmail.com>
2023-07-21 23:03:40 +0800

    chore: Renamed a component

---
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/MobileIcon.tsx
@@ -0,0 +1,18 @@
+function MobileIcon() {
+    return(
+        <>
+            <div className="w-3/5 h-3/5 absolute left-[20%] bottom-[44%]">
+                <div className="relative h-full w-full flex justify-center items-center">
+                    <div className="z-10 h-60 w-60">
+                        <img src="./assets/icons/illustration-woman-online-mobile.svg" />
+                    </div>
+                    <div className="z-0 h-60 w-60 absolute top-[48%]">
+                        <img src="./assets/icons/bg-pattern-mobile.svg" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MobileIcon;
