d46517ea61ba8c852d99bff415dc9281831c5051
Kian Candelario <kiancandelario1@gmail.com>
2023-07-21 23:16:15 +0800

    feat: Added hooks for responsiveness

--- Newbie/FAQ-Accordion/faqAccordion/src/components/Container.tsx
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Container.tsx
@@ -1,13 +1,24 @@
-import Icon from './Icon.tsx';
+import { useState, useEffect } from 'react';
+import MobileIcon from './MobileIcon.tsx';
 import Contents from './Contents.tsx';
 
 function Container() {
+  const [currWidth, setCurrWidth] = useState(window.innerWidth);
+
+  const resized = () => {
+    setCurrWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', resized);
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-b from-soft-violet-g to-soft-blue-g">
         <div className='h-full w-full flex justify-center items-center lg:grid lg:grid-flow-row lg:grid-cols-7'>
-          <Icon />
-          <div className='w-[90%] h-[75%] flex items-end lg:col-start-2 lg:col-span-5'>
+          {currWidth < 1024 ? <MobileIcon /> : ''}
+          <div className='w-[90%] h-[75%] flex items-end md:w-[70%] lg:w-full lg:col-start-2 lg:col-span-5'>
             <Contents />
           </div>
         </div>
