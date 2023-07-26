7c9d97de295a44189890fbd5c4d80580c8cccbd1
Kian Candelario <kiancandelario1@gmail.com>
2023-07-26 21:16:55 +0800

    style: Centered the items in large devices

--- Newbie/FAQ-Accordion/faqAccordion/src/components/Container.tsx
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Container.tsx
@@ -18,7 +18,7 @@ function Container() {
       <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-b from-soft-violet-g to-soft-blue-g">
         <div className='h-full w-full flex justify-center items-center lg:grid lg:grid-flow-row lg:grid-cols-7'>
           {currWidth < 1024 ? <MobileIcon /> : ''}
-          <div className='w-[90%] h-[75%] flex items-end md:w-[70%] lg:w-full lg:col-start-2 lg:col-span-5'>
+          <div className='w-[90%] h-[75%] flex items-end md:w-[70%] lg:w-full lg:col-start-2 lg:col-span-5 lg:items-center'>
             <Contents />
           </div>
         </div>
