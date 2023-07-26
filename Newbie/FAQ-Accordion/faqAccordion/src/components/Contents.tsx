128ee91bb24fba58e09ac653786bc7f7fb8081b8
Kian Candelario <kiancandelario1@gmail.com>
2023-07-26 21:20:01 +0800

    feat: Added a Desktop icon

--- Newbie/FAQ-Accordion/faqAccordion/src/components/Contents.tsx
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Contents.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import DesktopIcon from './DesktopIcon.tsx';
 
 function Contents() {
     const [i, setI] = useState<number | null>(null);
@@ -47,22 +46,30 @@ function Contents() {
     return (
         <>
             <div className="w-full h-[85%] flex">
-                <div className="w-full h-full bg-white rounded-3xl p-5 font-kumbh-sans lg:grid lg:grid-cols-5">
-                    {currWidth > 1024 ? 
-                        <div className='col-start-1 col-span-2'>
-                            <DesktopIcon />
+                <div className="w-full h-full bg-white rounded-3xl font-kumbh-sans lg:grid lg:grid-cols-6 lg:overflow-x-hidden">
+                    {currWidth >= 1024 ? 
+                        <div className='col-start-1 col-span-3 row-span-2 bg-[right_.5em_bottom_-11em] bg-no-repeat mr-16' style={
+                            {backgroundImage: "url(../assets/icons/bg-pattern-desktop.svg)"}
+                        }>
+                            <div className='relative h-auto w-[22rem] top-[19%] left-[-18%]'>
+                                <img src="../assets/icons/illustration-woman-online-desktop.svg" />
+                            </div>
+                            <div className='absolute top-[18.7rem] left-[5rem] w-36 h-auto'>
+                                    <img src="../assets/icons/illustration-box-desktop.svg" />
+                            </div>
                         </div>
                     : ''}
-                    <div className="text-3xl font-bold mt-28 mb-5 flex justify-center items-center lg:col-start-3 lg:mt-5 lg:block lg:mb-0">
+                    <div className="text-3xl font-bold mt-32 mb-5 flex justify-center items-center lg:col-start-4 lg:mt-10 lg:block lg:mb-5">
                         <h1>FAQ</h1>
                     </div>
-                    <div className="h-[55%] w-full text-xs overflow-y-auto lg:col-start-3 lg:col-span-3 lg:pr-10 lg:h-[97%] xl:text-sm">
+                    <div className="h-[50%] w-full text-xs overflow-y-auto px-5 lg:px-0 lg:col-start-4 lg:col-span-3 lg:pr-10 lg:h-[90%] xl:text-sm">
                         {
                             contents.map((content, index) => (
                                 <div key={content.id}>
                                     <div className={
-                                        `flex justify-between items-center py-5 lg:col-start-3
-                                        ${index === i ? 'border-b-0' : 'border-b-2'}`} onClick={() => {
+                                        `flex justify-between items-center py-5 lg:pb-5 lg:py-0 lg:col-start-3
+                                        ${index === i ? 'border-b-0' : 'border-b-2 mb-5'}`} 
+                                        onClick={() => {
                                         clicked(index)
                                     }}>
                                         <p className={`${index === i ? 'font-bold' : 'font-normal'}`}>{content.question}</p>
@@ -71,7 +78,7 @@ function Contents() {
                                         </span>
                                     </div>
                                 <div className={
-                                    `text-dark-grayish-blue lg:col-start-3 ${index === i ? 'block border-b-2 pb-5' : 'hidden border-b-0 pb-0'}`
+                                    `text-dark-grayish-blue lg:col-start-3 ${index === i ? 'block border-b-2 pb-5 lg:mb-5' : 'hidden border-b-0 pb-0'}`
                                     }>{content.answer}</div>
                                 </div>            
                             ))
