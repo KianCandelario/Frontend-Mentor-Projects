138d59fb32c12d59e7c09923fb9f907eda6e445a
Kian Candelario <kiancandelario1@gmail.com>
2023-07-29 17:49:10 +0800

    style: Changed the color of the texts and dividers

--- Newbie/FAQ-Accordion/faqAccordion/src/components/Contents.tsx
+++ Newbie/FAQ-Accordion/faqAccordion/src/components/Contents.tsx
@@ -59,7 +59,7 @@ function Contents() {
                             <DesktopIcon />
                         </div>
                     : ''}
-                    <div className="text-3xl font-bold mt-32 mb-5 flex justify-center items-center lg:col-start-4 lg:mt-10 lg:justify-start">
+                    <div className="text-3xl font-bold mt-32 mb-5 flex justify-center items-center text-vdark-desaturated-blue lg:col-start-4 lg:mt-10 lg:justify-start">
                         <h1>FAQ</h1>
                     </div>
                     <div className="h-[50%] w-full text-xs overflow-y-auto px-5 lg:px-0 lg:col-start-4 lg:col-span-4 lg:pr-10 lg:h-[90%] xl:text-sm">
@@ -67,19 +67,19 @@ function Contents() {
                             contents.map((content, index) => (
                                 <div key={content.id}>
                                     <div className={
-                                        `flex justify-between items-center py-5 lg:pb-5 lg:py-0 lg:col-start-3
-                                        ${index === i ? 'border-b-0' : 'border-b-2 lg:mb-5'} ${
+                                        `flex justify-between items-center py-5 text-vdark-grayish-blue lg:pb-5 lg:py-0 lg:col-start-3
+                                        ${index === i ? 'border-b-0' : 'border-b-2 border-light-grayish-blue lg:mb-5'} ${
                                         index === contents.length - 1 && i === null ? 'lg:mb-0' : '' /*Remove the margin bottom of the last child element */ }`} 
                                         onClick={() => {
                                         clicked(index)
                                     }}>
-                                        <p className={`${index === i ? 'font-bold' : 'font-normal'}`}>{content.question}</p>
+                                        <p className={`${index === i ? 'font-bold' : 'font-normal'} hover:text-soft-red hover:cursor-pointer`}>{content.question}</p>
                                         <span className={index === i ? 'rotate-180' : 'rotate-0'}>
                                         <img src="./assets/icons/icon-arrow-down.svg" />
                                         </span>
                                     </div>
                                     <div className={
-                                        `text-dark-grayish-blue lg:col-start-3 ${index === i ? 'block border-b-2 pb-5 lg:mb-5' : 'hidden border-b-0 pb-0'}`
+                                        `text-dark-grayish-blue lg:col-start-3 ${index === i ? 'block border-b-2 pb-5 border-light-grayish-blue lg:mb-5' : 'hidden border-b-0 pb-0'}`
                                     }>{content.answer}</div>
                                 </div>            
                             ))
