c0212de3ed8586eaf1e14332b2b71dd702790a12
Kian Candelario <kiancandelario1@gmail.com>
2023-07-31 21:10:43 +0800

    style: Added drop shadow and margin bottom

--- Junior/Advice-Generator/adviceGenerator/src/components/Card.tsx
+++ Junior/Advice-Generator/adviceGenerator/src/components/Card.tsx
@@ -3,7 +3,7 @@ import Contents from './Contents.tsx';
 function Card() {
     return (
         <>
-            <div className="w-[90%] h-[48.5%] bg-dark-grayish-blue rounded-xl">
+            <div className="w-[90%] h-[48.5%] mb-14 bg-dark-grayish-blue rounded-xl drop-shadow-2xl">
                 <Contents />
             </div>
         </>
