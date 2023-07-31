148efb8d2a8c30a8f954b36cc8880c6de78be427
Kian Candelario <kiancandelario1@gmail.com>
2023-07-31 17:42:10 +0800

    feat: Added custom themes

--- Junior/Advice-Generator/adviceGenerator/tailwind.config.js
+++ Junior/Advice-Generator/adviceGenerator/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-cyan': 'hsl(193, 38%, 86%)',
+        'neon-green': 'hsl(150, 100%, 66%)',
+        'grayish-blue': 'hsl(217, 19%, 38%)',
+        'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+        'dark-blue': 'hsl(218, 23%, 16%)',
+      },
+      fontFamily: {
+        'manrope' : ['manrope','sans-serif']
+      },
+      fontSize: {
+        'quote' : '28px'
+      }
+    },
   },
   plugins: [],
 }
