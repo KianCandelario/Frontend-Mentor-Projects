169bed7211011d51b31d979eb7eee3d25aa40049
Kian Candelario <kiancandelario1@gmail.com>
2023-08-06 22:00:52 +0800

    feat: Added responsiveness and hover effects

--- Junior/Advice-Generator/adviceGenerator/src/components/Contents.tsx
+++ Junior/Advice-Generator/adviceGenerator/src/components/Contents.tsx
@@ -10,6 +10,7 @@ interface apiResponse {
 function Contents() {
     const [items, setItems] = useState<apiResponse>({ slip: { id: 0, advice: '' } });;
     const [clicked, setClicked] = useState(false);
+    const [currWidth, setCurrWidth] = useState(window.innerWidth);
 
     const fetchingApi = async () => {
         const apiRequest = await fetch('https://api.adviceslip.com/advice');
@@ -25,23 +26,40 @@ function Contents() {
     const adviceNum = items.slip.id;
     const advice = items.slip.advice;
 
+    const setWidth = () => {
+        setCurrWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', () => setWidth());
+
+        return window.removeEventListener('resize', () => setWidth());  // Cleanup
+    });
+
 
     return (
         <>
             <div className="w-full h-full">
-                <div className="h-[15%] font-manrope text-neon-green text-xs tracking-[.25rem] flex justify-center items-end">
+                <div className="h-[15%] font-manrope text-neon-green text-xs tracking-[.25rem] flex justify-center items-end xl:h-[17%]">
                     <h4>ADVICE #{adviceNum}</h4>
                 </div>
-                <div className="h-[55%] text-light-cyan font-manrope text-[23px] text-center px-5 my-3 flex justify-center items-center">
+                <div className="h-[55%] text-light-cyan font-manrope text-[23px] text-center px-5 my-3 flex justify-center items-center xl:h-[53%] xl:px-8">
                     <q className='text-mobile-quote'>
                         {advice}
                     </q>
                 </div>
-                <div className="h-[13%] flex justify-center items-start">
-                    <img src="../../src/assets/icons/pattern-divider-mobile.svg" />
-                </div>
+                {
+                    currWidth < 550 ? 
+                    <div className="h-[13%] flex justify-center items-start">
+                        <img src="../../src/assets/icons/pattern-divider-mobile.svg" />
+                    </div>
+                    :
+                    <div className="h-[13%] flex justify-center items-start">
+                        <img src="../../src/assets/icons/pattern-divider-desktop.svg" />
+                    </div>
+                }
                 <div className='flex justify-center items-center'>
-                    <div className="z-10 bg-neon-green p-4 rounded-full shadow-2xl" onClick={() => setClicked(!clicked)}>
+                    <div className="z-10 bg-neon-green p-4 rounded-full shadow-2xl cursor-pointer hover:drop-shadow-green-glow" onClick={() => setClicked(!clicked)}>
                         <img src="../../src/assets/icons/icon-dice.svg" />
                     </div>
                 </div>
