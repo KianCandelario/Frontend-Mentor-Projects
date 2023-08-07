83bee0adeb225b26102e90ba990d27e36c415f9e
Kian Candelario <kiancandelario1@gmail.com>
2023-08-07 22:30:07 +0800

    style: Enhanced responsiveness

--- Junior/Advice-Generator/adviceGenerator/src/components/Card.tsx
+++ Junior/Advice-Generator/adviceGenerator/src/components/Card.tsx
@@ -3,7 +3,7 @@ import Contents from './Contents.tsx';
 function Card() {
     return (
         <>
-            <div className="w-[90%] h-[48.5%] mb-14 bg-dark-grayish-blue rounded-xl drop-shadow-2xl xl:w-[35%] xl:h-[43%]">
+            <div className="w-[90%] h-[48.5%] mb-14 bg-dark-grayish-blue rounded-xl drop-shadow-2xl sm:w-[70%] xmd:w-[60%] xl:w-[35%] xl:h-[43%]">
                 <Contents />
             </div>
         </>
